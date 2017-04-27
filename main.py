2ccd4a1e1e22375fbee481938234e6e5e1fd99ff
stmharry <harry19930924@yahoo.com.tw>
2017-04-27 21:00:09 +0800

    new algo

--- main.py
+++ main.py
@@ -13,39 +13,43 @@ app = flask.Flask(__name__)
 flask_bootstrap.Bootstrap(app)
 sess = requests.Session()
 
-def _(vec):
-    return vec / np.max(np.abs(vec))
+def _(vec, scale=1.0, ref_vec=None):
+    return (0.9 * vec / np.max(np.abs(ref_vec or vec)) + 1.0) * scale / 2.0
 
 
-def image_to_src(image_arr=None, image_pil=None):
-    if image_pil is None:
-        image_pil = PIL.Image.fromarray(image_arr)
-
+def to_src(image_arr=None, image_fig=None):
     f = cStringIO.StringIO()
-    image_pil.save(f, 'PNG')
+    if image_arr is not None:
+        image_pil = PIL.Image.fromarray(image_arr)
+        image_pil.save(f, 'png')
+    elif image_fig is not None:
+        image_fig.savefig(f, dpi=80, format='png')
+
     f.seek(0)
     return 'data:image/png;base64,{:s}'.format(base64.b64encode(f.read()))
 
 
 def image_to_output(image_rgb,
-                    pad_height,
-                    pad_width,
-                    filter_size,
-                    patch_size,
-                    sigma_detection,
-                    sigma_refinement,
-                    num_refinements):
+                    pad_height=30,
+                    pad_width=20,
+                    filter_size=10,
+                    patch_size=20,
+                    sigma_detection=3,
+                    sigma_refinement=6,
+                    num_refinements=1):
 
     output = {
-        'src': image_to_src(image_arr=image_rgb),
+        'src': to_src(image_arr=image_rgb),
+        'debug': [],
     }
 
     half_pad_height = pad_height / 2
     half_pad_width = pad_width / 2
     half_patch_size = patch_size / 2
 
-    image_gray = 100 - skimage.color.rgb2lab(image_rgb / 255.0)[:, :, 0]
-    (height, width) = image_gray.shape
+    (height, width, channel) = image_rgb.shape
+    image_lab = skimage.color.rgb2lab(image_rgb / 255.0)
+    image_gray = 100 - image_lab[:, :, 0]
 
     image_mean = np.mean(image_gray)
     image_gray = image_gray - image_mean
@@ -59,19 +63,61 @@ def image_to_output(image_rgb,
     )
 
     image_patch = image_gray[
-        half_pad_height:height,
-        half_pad_width:width,
+        half_pad_height:height + half_pad_height,
+        half_pad_width:width + half_pad_width,
     ]
     vec_w = np.mean(image_patch, axis=0)
     gf_w = scipy.ndimage.filters.gaussian_filter1d(vec_w, sigma=sigma_detection, mode='constant')
     gf_max_w = scipy.ndimage.filters.maximum_filter1d(gf_w, size=filter_size, mode='constant')
     ws = np.where(gf_w == gf_max_w)[0]
 
-
-    (fig, axes) = plot.subplots(1, 1, squeeze=False, figsize=(8, 6))
+    (fig, axes) = plot.subplots(1, 1, squeeze=False, figsize=(10, 3))
     ax = axes[0, 0]
-    ax.plot(_(gf_w), color='b')
+    ax.imshow(image_rgb)
+    ax.plot(_(vec_w, height), color='b')
+    ax.plot(_(gf_w, height), color='r')
+    ax.plot(ws, _(gf_w, height)[ws], color='r', linestyle='none', marker='o')
+    ax.set_xlim([0, width])
+    ax.set_ylim([height, 0])
+    output['debug'].append({'src': to_src(image_fig=fig)})
 
+    # DEBUG
+    (image_l, image_a, image_b) = np.split(image_lab, 3, axis=2)
+    image_lab = np.concatenate([
+        np.mean(image_l) - image_l,
+        image_a - np.mean(image_a),
+        image_b - np.mean(image_b),
+    ], axis=2)
+
+    image_lab = np.pad(
+        image_lab,
+        pad_width=[
+            (half_pad_height, half_pad_height),
+            (half_pad_width, half_pad_width),
+            (0, 0),
+        ],
+        mode='constant',
+    )
+    image_patch = image_lab[
+        half_pad_height:height + half_pad_height,
+        half_pad_width:width + half_pad_width,
+    ]
+    vec_w = np.mean(image_patch, axis=0)
+    gf2_l_w = scipy.ndimage.filters.gaussian_filter1d(vec_w[:, 0], sigma=3, axis=0, order=0, mode='constant')
+    gf2_ab_w = scipy.ndimage.filters.gaussian_filter1d(vec_w[:, 1:3], sigma=6, axis=0, order=2, mode='constant')
+    gf2_ab_w = np.sqrt(np.sum(np.square(gf2_ab_w), axis=1))
+    gf2_w = gf2_l_w * gf2_ab_w
+    gf2_max_w = scipy.ndimage.filters.maximum_filter1d(gf2_w, size=filter_size, mode='constant')
+    ws = np.where(gf2_w == gf2_max_w)[0]
+
+    (fig, axes) = plot.subplots(1, 1, squeeze=False, figsize=(10, 3))
+    ax = axes[0, 0]
+    # ax.plot(_(gf2_w), color='r')
+    # ax.plot(ws, _(gf2_w)[ws], color='r', linestyle='none', marker='o')
+    ax.plot(_(gf2_l_w), color='g')
+    ax.plot(_(gf2_ab_w), color='b')
+    ax.set_xlim([0, width])
+    output['debug'].append({'src': to_src(image_fig=fig)})
 
     output['patches'] = []
     for w in ws:
@@ -121,7 +167,7 @@ def image_to_output(image_rgb,
         image_patch = np.uint8((image_patch + image_mean) / 100 * 255)
 
         patch['pos'] = (h, w)
-        patch['src'] = image_to_src(image_arr=image_patch)
+        patch['src'] = to_src(image_arr=image_patch)
         output['patches'].append(patch)
 
     return output
@@ -133,22 +179,12 @@ def main():
     r = sess.get('http://railway.hinet.net/ImageOut.jsp')
     image_pil = PIL.Image.open(cStringIO.StringIO(r.content))
     image_rgb = np.array(image_pil)
+
     '''
 
-    npy = np.load('/Users/harry/Dropbox/dev/boleto2/img/imgs.npy')[10:30]
-
-    outputs = []
-    for (num_image, image_rgb) in enumerate(npy):
-        outputs.append(image_to_output(
-            image_rgb,
-            pad_height=30,
-            pad_width=20,
-            filter_size=15,
-            patch_size=20,
-            sigma_detection=3,
-            sigma_refinement=6,
-            num_refinements=1,
-        ))
+    image_rgb = np.load('imgs.npy')[13]
+    output = image_to_output(image_rgb)
+    outputs = [output]
 
     return flask.render_template(
         'index.html',
